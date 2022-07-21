8c6506936783494e1f1df747eac9bde383636216
chrischristakis <chriscchristakis@gmail.com>
2022-07-21 17:20:38 -0400

    Added max subarray solution:

--- problems/ArrayProblems.py
+++ problems/ArrayProblems.py
@@ -50,6 +50,32 @@ def twoSum2(nums, target):
             return [i, map[nums[i]]]
         map[diff] = i
 
+
+# Wayyy too slow!
+def maxSubArray1(nums):
+    # Time complexity O(n^2)
+    max_sum = float('-inf')
+    for i in range(0, len(nums)):
+        local_sum = 0
+        for j in range(i, len(nums)):
+            local_sum += nums[j]
+            max_sum = max(local_sum, max_sum)
+    return max_sum
+
+# Kadanes algorithim O(n)!!
+def maxSubArray2(nums):
+    # Basically, at any nums[i], if we knew what the greatest sum so far before nums[i] was, so max_sum[i-1], then
+    # The largest sum will either max(nums[i], nums[i]+max_sum[i-1]). We then compare this to our global max_sum, and
+    # Store the largest result.
+
+    global_max = float('-inf')
+    curr_max = 0
+    for num in nums:
+        curr_max = max(num, num + curr_max)
+        global_max = max(curr_max, global_max)
+    return global_max
+
+
 str = "Hi I'm Chris"
 print(p1s1(str))
 print(p1s2(str))
@@ -60,3 +86,7 @@ print(p2s1(a1, a2))
 nums = [2, 7, 11, 15]
 print(twoSum1(nums, 9))
 print(twoSum2(nums, 9))
+
+nums = [-2, 1, -3, 4, -1, 2, 1, -5, 4]
+print(maxSubArray1(nums))
+print(maxSubArray2(nums))
